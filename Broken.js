4df86bf18ad6d5ed5c048a64d361f94a264336ba
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-29 15:05:37 -0700

    Add files via upload

--- Broken.js
+++ Broken.js
@@ -1,31 +1,16 @@
 (function(){
 window.addEventListener('load', function(){
-	var requestorControlId = 111041865, /** ID REQUESTOR **/
-	affectedControlId = 111045282, 	/** ID OF AFFECTED USER FIELD, USUALLY SAME AS REQUESTOR **/
-	isDoctorControlId = 111073787,  /** ID OF WHO THIS TICKET IS FOR, WE WILL CHECK THIS FOR DOCTOR **/
-	impactControlId = 111090343,    /** ID OF NUMBER IMPACTED, WE WILL CHECK THIS FOR MORE THAN 1 USER **/
-	priorityControlId = 111115973   /** ID OF PRIORITY, WE WILL SET THIS BASED ON CRITERIA **/
-
-	sourceControlInstance = loader.getEngine().getDocument().getElementById(requetorControlId);
-	targetControlInstance = loader.getEngine().getDocument().getElementById(affectedControlId);
-	isDoctorControlInstance = loader.getEngine().getDocument().getElementById(isDoctorControlId);
-	impactControlInstance = loader.getEngine().getDocument().getElementById(impactControlId);
-	priorityControlInstance = loader.getEngine().getDocument().getElementById(priorityControlId);
-/**
+	var sourceControlId = 111041865, /** ID SOURCE FIELD **/
+	targetControlId = 111045282, 	/** ID OF TARGET FIELD **/
+	
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+	
 	sourceControlInstance.on('value-change', function(){
 		targetControlInstance.setValue( sourceControlInstance.getValue() );
 	});
- **/
-    isDoctorControlInstance.on('value-change', function() {
-		targetControlInstance.setValue( sourceControlInstance.getValue() );
-		//s = isDoctorControlInstance.getValue();
-		//if (s.indexOf("doctor") !== -1) {
-		//	targetControlInstance.setValue( sourceControlInstance.getValue() );
-		//}
-	});
 	
 	targetControlInstance.setValue( sourceControlInstance.getValue() );
-	
 	
 });
 })();
