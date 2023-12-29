4880d1f44f3451c4bb3c4e5867f8550e380e27aa
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-29 16:14:47 -0700

    Add files via upload

--- Broken.js
+++ Broken.js
@@ -12,16 +12,25 @@ window.addEventListener('load', function(){
 	
 	targetControlInstance.setValue( sourceControlInstance.getValue() );
 	
+	
+	
+});
+window.addEventListener('load', function(){
+	var sourceControlId = 111115973, /** ID SOURCE FIELD **/
+	targetControlId = 111117248, 	/** ID OF TARGET FIELD **/
+	
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+	
+	sourceControlInstance.on('value-change', function(){
+		targetControlInstance.setValue( sourceControlInstance.getValue() );
+	});
+	
+	targetControlInstance.setValue( sourceControlInstance.getValue() );
+	
+	
+	
 });
 })();
 
-function setPriority() {
-	var sourceControlId = 111090343, /** ID OF IMPACTED USERS FIELD **/
-	targetControlId = 111115973 	/** ID OF PRIORITY FIELD **/
-	var result1 = loader.engine.document.getElementById(sourceControlId).getProperty('value.value'); 
-	
-	loader.engine.document.getElementById(111115973).setValue(({"value": result1})); 
-};
-
-window.onchange = setPriority;
 
