591069cf3209dbe6e94e27b212ae8960dbb7d94a
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-29 16:24:20 -0700

    Add files via upload

--- Broken.js
+++ Broken.js
@@ -10,27 +10,19 @@ window.addEventListener('load', function(){
 		targetControlInstance.setValue( sourceControlInstance.getValue() );
 	});
 	
-	targetControlInstance.setValue( sourceControlInstance.getValue() );
-	
-	
-	
-});
-window.addEventListener('load', function(){
-	var sourceControlId = 111115973, /** ID SOURCE FIELD **/
-	targetControlId = 111117248, 	/** ID OF TARGET FIELD **/
-	
-	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
-	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
-	
-	sourceControlInstance.on('value-change', function(){
-		targetControlInstance.setValue( sourceControlInstance.getValue() );
+    sourceControlInstance2 = loader.getEngine().getDocument().getElementById(111115973);
+	targetControlInstance2 = loader.getEngine().getDocument().getElementById(111117248);
+
+	sourceControlInstance2.on('value-change', function(){
+		targetControlInstance2.setValue( sourceControlInstance2.getValue() );
 	});
-	
+
 	targetControlInstance.setValue( sourceControlInstance.getValue() );
-	
+	targetControlInstance2.setValue( sourceControlInstance2.getValue() );
 	
 	
 });
+
 })();
 
 
