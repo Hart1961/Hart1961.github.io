863c411fd07b9ddff9f3febba1dd0cb79374d396
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2023-12-29 13:18:11 -0700

    Update Broken.js

--- Broken.js
+++ Broken.js
@@ -14,3 +14,24 @@ window.addEventListener('load', function(){
 	targetControlInstance.setValue( sourceControlInstance.getValue() );
 });
 })();
+(function(){
+window.addEventListener('load', function(){
+	var sourceControlId = 111073787, /** ID OF THE SENDER CONTROL **/
+	targetControlId = 111115973 /** ID OF THE RECEIVER CONTROL **/
+	
+
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+
+	sourceControlInstance.on('value-change', function(){
+		if (sourceControlInstance.getValue() === "Me, I'm a doctor") {
+			targetControlInstance.setValue( "High" );
+		}
+		if (sourceControlInstance.getValue() === "Someone else who is a doctor") {
+			targetControlInstance.setValue( "High");
+		}	
+	});
+
+	targetControlInstance.setValue( sourceControlInstance.getValue() );
+});
+})();
