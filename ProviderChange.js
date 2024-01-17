bf2d2e0f4b7e3be56133803440f40fa2c28cec61
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2024-01-17 12:08:27 -0700

    Create ProviderChange.js

---
+++ ProviderChange.js
@@ -0,0 +1,17 @@
+(function(){
+window.addEventListener('load', function(){
+	var sourceControlId = 110705755, /** ID OF Last Official day with ACP **/
+	targetControlId = 110721038 /** ID OF Date to route results **/
+	
+	window.alert("I got called");
+
+	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
+
+	sourceControlInstance.on('value-change', function(){
+		targetControlInstance.setValue( sourceControlInstance.getValue() );
+	});
+
+	targetControlInstance.setValue( sourceControlInstance.getValue() );
+});
+})();
