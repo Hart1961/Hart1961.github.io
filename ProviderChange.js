cdbc14a554dcabb15737fa57131f576cc1a1a76e
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2024-01-17 12:57:17 -0700

    Update ProviderChange.js

--- ProviderChange.js
+++ ProviderChange.js
@@ -1,17 +1,20 @@
 (function(){
 window.addEventListener('load', function(){
 	var sourceControlId = 110705755, /** ID OF Last Official day with ACP **/
-	targetControlId = 111435706 /** ID OF Date to route results **/
+	targetControlId = 111435706; /** ID OF Date to route results **/
+	var d = new Date();
 	
-	window.alert("I got called");
+	window.alert("Call 2");
 
 	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
 	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
 	sourceControlInstance.on('value-change', function(){
-		targetControlInstance.setValue( sourceControlInstance.getValue() + 1 );
+		d.setValue( sourceControlInstance.getValue());
+		targetControlInstance.setValue( d.getValue());
+		/* targetControlInstance.setValue( sourceControlInstance.getValue()); */
 	});
-
-	targetControlInstance.setValue( sourceControlInstance.getValue() + 1 );
+		targetControlInstance.setValue( d.getValue());
+	/* targetControlInstance.setValue( sourceControlInstance.getValue()); */
 });
 })();
