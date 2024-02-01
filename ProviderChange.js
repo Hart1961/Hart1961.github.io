c40d2c39220f150c9865797fa40de19b4d633673
Hart1961 <153132453+Hart1961@users.noreply.github.com>
2024-02-01 16:04:11 -0700

    Add files via upload

--- ProviderChange.js
+++ ProviderChange.js
@@ -1,26 +1,27 @@
 (function(){
 window.addEventListener('load', function(){
-	var sourceControlId = 110705755, /** ID OF Last Official day with ACP **/
-	targetControlId = 111435706; /** ID OF Date to route results **/
+	var sourceControlId = 110705761, /* ID OF number of days for access */
+		dateSourceControlId = 110705755, /* ID OF Last Official day with ACP */
+		targetControlId = 111435706; /** ID OF Date to route results **/
+	var n = 0;
 	var d = new Date();
 	
-	window.alert("Call 9");
-	window.alert(d);
-
 	sourceControlInstance = loader.getEngine().getDocument().getElementById(sourceControlId);
+	dateSourceControlInstance = loader.getEngine().getDocument().getElementById(dateSourceControlId);
 	targetControlInstance = loader.getEngine().getDocument().getElementById(targetControlId);
 
 	sourceControlInstance.on('value-change', function(){
 		
-		window.alert( sourceControlInstance.getProperty('value.value'));
-		d = sourceControlInstance.getProperty('value.value');
-		window.alert(d);
+		window.alert(sourceControlInstance.getProperty('value.value'));
+		n = sourceControlInstance.getProperty('value.value');
+		window.alert(n);
 		d.setDate('1-01-2024');
 		window.alert(d);
-		targetControlInstance.setValue( d.getValue());
+		window.alert(dateSourceControlInstance.getProperty('value.value'));
+		/* targetControlInstance.setValue( d.getValue());
 		/* targetControlInstance.setValue( sourceControlInstance.getValue()); */
 	});
-		targetControlInstance.setValue( d.getValue());
+		/* targetControlInstance.setValue( d.getValue());
 	/* targetControlInstance.setValue( sourceControlInstance.getValue()); */
 });
 })();
